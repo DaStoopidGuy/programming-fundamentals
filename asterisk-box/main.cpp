deaea3f0c226c7a3dc23b780cdeca29a1bc19aba
DaStoopidGuy <dastoopidguy@protonmail.com>
2024-09-25 08:02:58 +0500

    asterisk box first iteration done.

---
+++ asterisk-box/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\t\t\t\t**************************" << endl;
+    cout << "\t\t\t\t*      Introduction      *" << endl;
+    cout << "\t\t\t\t*   Name: Sareb Janjua   *" << endl;
+    cout << "\t\t\t\t*   Degree: BSCS         *" << endl;
+    cout << "\t\t\t\t*   Semester: 1st        *" << endl;
+    cout << "\t\t\t\t**************************" << endl;
+    return 0;
+}
